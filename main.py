916763dd8c42d97699b56981c0560b552090a1df
Andrei Polyakov <ap120368@gmail.com>
2022-08-18 16:17:07 +0500

    PEP8 fixed

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from __future__ import print_function
+
 from sys import argv
 
+from google.oauth2 import service_account
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
-from google.oauth2 import service_account
+
 from frigate import Frigate
 from selenium_frigate import selenium_frigate
 
@@ -11,31 +13,36 @@ SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 
 SPREADSHEET_ID = '1HFbeHuiU_QNT45DSohVMw548EiFkgGSHyKdtafSvVXw'
 
+
 def main():
     """
-    Заполняет google таблицу. Secure file должен быть свой и email от Service Access Key должен быть 
-    добавлен в список кому разрешено править файл.
-    Secure_file - must be your own and email for the Service Access Key must be added to access settings with edit right
-    to google sheet 
+    Заполняет google таблицу. Secure file должен быть свой и email от
+    Service Access Key должен быть добавлен в список кому разрешено
+    править файл.
+    Secure_file - must be your own and email for the Service Access Key must
+    be added to access settings with edit right to google sheet.
     """
     is_Selenium = True if len(argv) == 1 else False
 
     secure_file = 'sf.json'
-    tables = ['117','138','161']
-    if is_Selenium: 
+    tables = ['117', '138', '161']
+    if is_Selenium:
         html_tables = selenium_frigate(tables)
-        if html_tables == None:
+        if html_tables is None:
             print('Ошибка: Не удалось получить получить таблицы')
             return
-    else: 
+    else:
         frigate = Frigate()
-        # Login is not really needed. We need id and secret for modem page request.
-        # We can get id and secret form login or simply view the values in the browser in developer mode.
-        # Values are rarely changed.
-        #frigate.login() 
-    
+        # Login is not really needed. We need id and
+        # secret for modem page request.
+        # We can get id and secret form login or simply view the values
+        # in the browser in developer mode. Values are rarely changed.
+        # frigate.login()
     creds = None
-    creds = service_account.Credentials.from_service_account_file(secure_file, scopes=SCOPES)
+    creds = service_account.Credentials.from_service_account_file(
+        secure_file,
+        scopes=SCOPES
+    )
 
     try:
         service = build('sheets', 'v4', credentials=creds)
@@ -44,32 +51,55 @@ def main():
         sheet = service.spreadsheets()
         data = []
         for i, t in enumerate(tables):
-            if is_Selenium: data.append({'range': f'{t}!B3', 'values': html_tables[i]})
-            else: data.append({'range': f'{t}!B3', 'values': frigate.table(t)})
+            if is_Selenium:
+                data.append({'range': f'{t}!B3', 'values': html_tables[i]})
+            else:
+                data.append({'range': f'{t}!B3', 'values': frigate.table(t)})
         body = {'valueInputOption': 'RAW', 'data': data}
-        res = sheet.values().batchUpdate(spreadsheetId=SPREADSHEET_ID, body=body).execute()
-        print(f'Обновлено: {res["totalUpdatedCells"]} ячеек на {res["totalUpdatedSheets"]} листах')
+        res = sheet.values().batchUpdate(
+            spreadsheetId=SPREADSHEET_ID,
+            body=body
+        ).execute()
+        print((
+            f'Обновлено: {res["totalUpdatedCells"]} ячеек'
+            f' на {res["totalUpdatedSheets"]} листах'
+        ))
         # Conditional formatting
         sheets_ids = [0, 1292097999, 729629251]
         requests = []
         for id in sheets_ids:
-            ranges = [{'sheetId': id, 'startRowIndex': 2, 'startColumnIndex': 3, 'endColumnIndex': 4}]
+            ranges = [{
+                'sheetId': id,
+                'startRowIndex': 2,
+                'startColumnIndex': 3,
+                'endColumnIndex': 4
+            }]
             requests.append({
                 'addConditionalFormatRule': {
                     'rule': {
                         'ranges': ranges,
                         'booleanRule': {
                             'condition': {'type': 'NOT_BLANK'},
-                            'format': {'backgroundColor': {'red': 0.1, 'green': 0.6, 'blue': 0.6}
+                            'format': {
+                                'backgroundColor': {
+                                    'red': 0.1,
+                                    'green': 0.6,
+                                    'blue': 0.6
+                                }
                             }
                         }
                     },
                     'index': 0
                 }
             })
-        res = sheet.batchUpdate(spreadsheetId=SPREADSHEET_ID, body={'requests': requests}).execute()
+        res = sheet.batchUpdate(
+            spreadsheetId=SPREADSHEET_ID,
+            body={'requests': requests}
+        ).execute()
 
     except HttpError as err:
         print(err)
 
-main()
+
+if __name__ == '__main__':
+    main()
--- selenium_frigate.py
+++ selenium_frigate.py
@@ -1,32 +1,40 @@
+import time
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-import time
+
 from frigate import Frigate
 
-def try_exc(driver, by, value, place, keys = None):
+
+def try_exc(driver, by, value, place, keys=None):
     """
     Проверяет доступность элемента, если не доступен, проверяет и закрывает
     ws-quiz-ifame и наконец обрабатывает начальный элемент
     """
     try:
-        el = driver.find_element(by=by,value=value)
+        el = driver.find_element(by=by, value=value)
     except Exception:
         if check_iframe(driver, place):
-            el = driver.find_element(by=by,value=value)
-        else: return None
+            el = driver.find_element(by=by, value=value)
+        else:
+            return None
     finally:
-        if keys: el.send_keys(keys)
-        else: el.click()
+        if keys:
+            el.send_keys(keys)
+        else:
+            el.click()
     return "Done"
+
 
 def check_iframe(driver, place):
     """
-    Закрывает ws-quiz-ifame, если он есть. Возвращает True, если ws-quiz-iframe обработан.
+    Закрывает ws-quiz-ifame, если он есть. Возвращает True,
+    если ws-quiz-iframe обработан.
     """
     print(f'Exception: {place}')
-    try: 
+    try:
         driver.switch_to.frame('ws-quiz-iframe')
-        el = driver.find_element(by='class name',value='close-btn')
+        el = driver.find_element(by='class name', value='close-btn')
         el.click()
         driver.switch_to.default_content()
         return True
@@ -34,10 +42,13 @@ def check_iframe(driver, place):
         print('Exception: no ws-quiz-ifame')
         return False
 
-def selenium_frigate(tables, user='dockeep9@gmail.com', password='l4}$04|G') ->str:
+
+def selenium_frigate(tables, user='dockeep9@gmail.com',
+                     password='l4}$04|G') -> str:
     """
-    Реализует двухвакторную авторизацию и возвращает массив HTML таблиц (<table>...</table>) 
-    для запрашиваемых таблиц в параметре tables(массив строк)
+    Реализует двухвакторную авторизацию и возвращает массив HTML таблиц
+    (<table>...</table>) для запрашиваемых таблиц в параметре tables
+    (массив строк)
     """
     options = Options()
     options.add_argument('start-maximized')
@@ -45,39 +56,80 @@ def selenium_frigate(tables, user='dockeep9@gmail.com', password='l4}$04|G') ->s
     driver = webdriver.Chrome(options=options)
     driver.implicitly_wait(10)
     driver.get("https://frigate-proxy.ru/ru/type/all")
-    
-    #if try_exc(driver, 'class name', 'white-saas-generator-btn-cancel', 'white-saas-generator-btn-cancel') == None: return None
-        
-    if try_exc(driver, 'xpath', '//a[@href="come-in"]', 'Login') == None: return None
 
-    if try_exc(driver, 'xpath', '//div[@id="js_come-in"]/div[@class="popup_content"]/form[@class="popup-form"]/div[@class="inp-w"]/input[@name="email"]', 
-        'User', user) == None: return None
-    
-    if try_exc(driver, 'name', 'password', 'Password', password) == None: return None
+    if try_exc(driver, 'xpath', '//a[@href="come-in"]', 'Login') is None:
+        return None
+
+    if try_exc(
+        driver,
+        'xpath',
+        ('//div[@id="js_come-in"]/div[@class="popup_content"]'
+         '/form[@class="popup-form"]/'
+         'div[@class="inp-w"]/input[@name="email"]'),
+        'User',
+        user
+    ) is None:
+        return None
+
+    if try_exc(driver, 'name', 'password', 'Password', password) is None:
+        return None
+
+    ifrm_value = 'iframe[src^="https://www.google.com/recaptcha/api2/anchor"]'
 
     try:
-        driver.switch_to.frame(driver.find_element(by='css selector',value='iframe[src^="https://www.google.com/recaptcha/api2/anchor"]')) 
+        driver.switch_to.frame(
+            driver.find_element(
+                by='css selector',
+                value=ifrm_value
+            )
+        )
     except Exception:
         if check_iframe(driver, 'google'):
-            driver.switch_to.frame(driver.find_element(by='css selector',value='iframe[src^="https://www.google.com/recaptcha/api2/anchor"]')) 
-        else: return None
+            driver.switch_to.frame(
+                driver.find_element(
+                    by='css selector',
+                    value=ifrm_value
+                )
+            )
+        else:
+            return None
     finally:
-        anchor = driver.find_element(by='id',value='recaptcha-anchor')
+        anchor = driver.find_element(by='id', value='recaptcha-anchor')
         anchor.click()
     # Задержка на заполнение captcha
     time.sleep(40)
     driver.switch_to.default_content()
 
-    if try_exc(driver, 'xpath', '//div[@id="js_come-in"]/div[@class="popup_content"]/form[@class="popup-form"]/button[1]', 'Submit 1') == None: return None
+    if try_exc(
+        driver,
+        'xpath',
+        ('//div[@id="js_come-in"]/div[@class="popup_content"]'
+         '/form[@class="popup-form"]/button[1]'),
+        'Submit 1'
+    ) is None:
+        return None
 
     # Задержка на получение email
     time.sleep(15)
     frigate = Frigate()
     code = frigate.email_code()
 
-    if try_exc(driver, 'xpath', '//div[@class="filter-item"]/input[1]', 'Verification code', code) == None: return None
+    if try_exc(
+        driver,
+        'xpath'
+        '//div[@class="filter-item"]/input[1]',
+        'Verification code',
+        code
+    ) is None:
+        return None
 
-    if try_exc(driver, 'xpath', '//div[@class="forgot-pass-w"]/form[1]/button[1]', 'Submit 2') == None: return None
+    if try_exc(
+        driver,
+        'xpath',
+        '//div[@class="forgot-pass-w"]/form[1]/button[1]',
+        'Submit 2'
+    ) is None:
+        return None
 
     original_window = driver.current_window_handle
     res = []
@@ -89,4 +141,6 @@ def selenium_frigate(tables, user='dockeep9@gmail.com', password='l4}$04|G') ->s
         driver.switch_to.window(original_window)
     return res
 
-#selenium_frigate()
+
+if __name__ == '__main__':
+    selenium_frigate()
